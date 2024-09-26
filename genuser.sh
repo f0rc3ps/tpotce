9c18ba23fc054520c7eff21b18542337b8cecacb
r@bb1th01e <bugb0untypropentest@gmail.com>
2024-09-26 18:57:34 +0300

    Update genuser.sh

--- genuser.sh
+++ genuser.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -v $HOME/tpotce:/data --entrypoint bash -it -u $(id -u):$(id -g) dtagdevsec/tpotinit:24.04 "/opt/tpot/bin/genuser.sh"
+docker run -v $HOME/tpotce:/data --entrypoint bash -it -u $(id -u):$(id -g) f0rc3ps/tpotinit:24.04 "/opt/tpot/bin/genuser.sh"
